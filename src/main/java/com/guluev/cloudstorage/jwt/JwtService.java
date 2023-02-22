02b9b01717389967e50356c1fc87cd9ec4400328
Tural Guluev <tural-98@bk.ru>
2023-02-22 16:34:07 +0300

    added secret_key in application.yaml and implemented the getSigningKey method

--- src/main/java/com/guluev/cloudstorage/jwt/JwtService.java
+++ src/main/java/com/guluev/cloudstorage/jwt/JwtService.java
@@ -2,10 +2,17 @@ package com.guluev.cloudstorage.jwt;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 
+import java.security.Key;
 import java.util.function.Function;
 
 public class JwtService {
+    @Value("#{secret_key}")
+    private static String SECRET_KEY;
+
     public String extractUserEmail(String token) {
         return extractClaim(token, Claims::getSubject);
     }
@@ -17,6 +24,12 @@ public class JwtService {
 
     private Claims extractAllClaims(String token) {
         return Jwts.parserBuilder().setSigningKey(getSigningKey()).build().parseClaimsJws(token).getBody();
+    }
+    
+
+    private Key getSigningKey() {
+        var currentKey = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(currentKey);
     }
 
 }
