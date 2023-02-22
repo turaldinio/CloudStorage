69f17ec301655b4edad1e3cb2e208262c50ee9f8
Tural Guluev <tural-98@bk.ru>
2023-02-22 16:22:41 +0300

    added extractClaim method in JwtService

--- src/main/java/com/guluev/cloudstorage/jwt/JwtService.java
+++ src/main/java/com/guluev/cloudstorage/jwt/JwtService.java
@@ -2,8 +2,16 @@ package com.guluev.cloudstorage.jwt;
 
 import io.jsonwebtoken.Claims;
 
+import java.util.function.Function;
+
 public class JwtService {
     public String extractUserEmail(String token) {
         return extractClaim(token, Claims::getSubject);
     }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        var claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
 }
