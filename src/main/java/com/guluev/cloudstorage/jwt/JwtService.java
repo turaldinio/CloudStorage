2a44e598e222ac58aceae2806e9a09cd2d805339
Tural Guluev <tural-98@bk.ru>
2023-02-22 16:19:52 +0300

    added jjwt dependency in pom.xml

--- src/main/java/com/guluev/cloudstorage/jwt/JwtService.java
+++ src/main/java/com/guluev/cloudstorage/jwt/JwtService.java
@@ -1,5 +1,7 @@
 package com.guluev.cloudstorage.jwt;
 
+import io.jsonwebtoken.Claims;
+
 public class JwtService {
     public String extractUserEmail(String token) {
         return extractClaim(token, Claims::getSubject);
