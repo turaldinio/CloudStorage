138a728082368da8decdc6e7c8b5d541a30bc87c
Tural Guluev <tural-98@bk.ru>
2023-02-22 15:50:10 +0300

    added JwtAuthFilter, (added the ability to authorize using a token)

---
+++ src/main/java/com/guluev/cloudstorage/jwt/JwtAuthFilter.java
@@ -0,0 +1,47 @@
+package com.guluev.cloudstorage.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader("auth-token");
+        if (authHeader == null) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        String token = authHeader.substring("auth-token".length());
+
+        String userEmail = jwtService.extractUserEmail(token);
+
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(userEmail);
+            if (jwtService.isTokenValid(token, userDetails)) {
+                var authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+        filterChain.doFilter(request, response);
+
+    }
+}
--- src/main/java/com/guluev/cloudstorage/security/SecurityConfig.java
+++ src/main/java/com/guluev/cloudstorage/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.guluev.cloudstorage.se
 curity;
 
+import com.guluev.cloudstorage.jwt.JwtAuthFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
