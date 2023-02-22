7c6dcf045d83ef5fff38047a8c22c4cdb2547021
Tural Guluev <tural-98@bk.ru>
2023-02-22 17:20:00 +0300

    added UserRepository interface and implement findUserByEmail method

---
+++ src/main/java/com/guluev/cloudstorage/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.guluev.cloudstorage.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUserByEmail(String email);
+}
