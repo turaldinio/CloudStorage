7e643b019d4e1ba6118bad4711fbacfb43a774b4
Tural Guluev <tural-98@bk.ru>
2023-02-22 13:31:35 +0300

    added SecurityConfig class (secirityFilterChain bean)

---
+++ src/main/java/com/guluev/cloudstorage/security/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.guluev.cloudstorage.se
+curity;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final AuthenticationProvider authenticationProvider;
+    private final JwtAuthFilter jwtAuthFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception {
+        security.
+                csrf().disable().
+                authorizeHttpRequests().
+                requestMatchers("/login").permitAll().
+                anyRequest().authenticated().
+                and().
+                sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).
+                and().
+                authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        return security.build();
+
+    }
+
+}
