c22bbd921b65cb8a9b864d6a818db179341e6d3c
Tural Guluev <tural-98@bk.ru>
2023-02-26 16:40:14 +0300

    added the ApplicationController

---
+++ src/main/java/com/guluev/cloudstorage/rest/ApplicationController.java
@@ -0,0 +1,35 @@
+package com.guluev.cloudstorage.rest;
+
+import com.guluev.cloudstorage.auth.service.AuthenticationRequest;
+import com.guluev.cloudstorage.auth.service.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+public class ApplicationController {
+
+    private final AuthenticationService authenticationService;
+
+    private final FileService fileService;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticate(@RequestBody AuthenticationRequest request) {
+        var result = authenticationService.authenticate(request);
+        return ResponseEntity.ok(Map.of("auth-token", result.getToken()));
+
+    }
+
+    @PostMapping(value = "/file", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<?> upload(MultipartFile file) {
+        fileService.uploadFile(file);
+        return ResponseEntity.ok("asd");
+    }
+}
