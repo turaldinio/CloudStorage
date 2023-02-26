087304651ab6319e13569dde1b7af81f21bee02a
Tural Guluev <tural-98@bk.ru>
2023-02-26 16:22:06 +0300

    added authController

---
+++ src/main/java/com/guluev/cloudstorage/rest/AuthController.java
@@ -0,0 +1,23 @@
+package com.guluev.cloudstorage.rest;
+
+import com.guluev.cloudstorage.jwt.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authorize(AuthenticationRequest request) {
+        var token = authenticationService.authenticate(request).getToken();
+        return ResponseEntity.ok(Map.of("auth-token", token));
+
+    }
+}
