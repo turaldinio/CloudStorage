c5d6b48dffc826a24f683b00b87b58d84c466a5d
Tural Guluev <tural-98@bk.ru>
2023-03-09 15:16:39 +0300

    added FileRepository methods

--- src/main/java/com/guluev/cloudstorage/repository/FileRepository.java
+++ src/main/java/com/guluev/cloudstorage/repository/FileRepository.java
@@ -2,8 +2,19 @@ package com.guluev.cloudstorage.repository;
 
 import com.guluev.cloudstorage.entity.UserFiles;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface FileRepository  extends JpaRepository<UserFiles,Long> {
+    int deleteByFileNameAndUserId(String fileName, long userId);
+
+    Optional<UserFiles> getByFileNameAndUserId(String fileName, long userId);
+
+    @Modifying
+    @Query(value = "update UserFiles uf set uf.fileName=?2 where uf.fileName=?1 and uf.userId=?3")
+    int update(String oldName, String newName, long userId);
 }
