5fd4046fb3a3e24a8f7856681fd2e82562c593a5
Tural Guluev <tural-98@bk.ru>
2023-03-09 15:27:47 +0300

    update controller class

--- src/main/java/com/guluev/cloudstorage/rest/ApplicationController.java
+++ src/main/java/com/guluev/cloudstorage/rest/ApplicationController.java
@@ -1,36 +1,62 @@
 package com.guluev.cloudstorage.rest;
 
-import com.guluev.cloudstorage.auth.AuthenticationRequest;
-import com.guluev.cloudstorage.auth.AuthenticationService;
+
+import com.guluev.cloudstorage.model.FileResponse;
 import com.guluev.cloudstorage.service.FileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
 public class ApplicationController {
+    private final FileService userFileService;
+    private final OperationService operationService;
 
-    private final AuthenticationService authenticationService;
-
-    private final FileService fileService;
-
-    @PostMapping("/login")
-    public ResponseEntity<?> authenticate(@RequestBody AuthenticationRequest request) {
-        var result = authenticationService.authenticate(request);
-        return ResponseEntity.ok(Map.of("auth-token", result.getToken()));
-
+    @GetMapping("/list")
+    public List<FileResponse> getFileListLimit(Integer limit) {
+        return userFileService.getFilesListLimit(limit);
     }
+
 
     @PostMapping(value = "/file", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
     public ResponseEntity<?> upload(MultipartFile file) {
-        fileService.uploadFile(file);
-        return ResponseEntity.ok("asd");
+        var result = userFileService.uploadFile(file);
+        operationService.addSuccessfulOperation(String.format("the file %s is uploaded", file.getOriginalFilename()));
+        return result;
     }
+
+    @DeleteMapping("/file")
+    public ResponseEntity<?> deleteFile(String filename) {
+        userFileService.deleteFile(filename);
+        operationService.addSuccessfulOperation(String.format("the file %s is deleted", filename));
+        return ResponseEntity.ok("Success deleted");
+    }
+
+
+    @PutMapping("/file")
+    public ResponseEntity<?> updateFileName(String filename, @RequestBody FileResponse fileResponse) {
+        userFileService.updateFileName(filename, fileResponse);
+        operationService.addSuccessfulOperation(String.format("the file was renamed from %s to %s", filename, fileResponse.getFilename()));
+
+        return ResponseEntity.ok("Success deleted");
+
+    }
+
+    @GetMapping("/file")
+    public ResponseEntity<?> downloadFile(String filename) {
+        var result = userFileService.downloadFileByName(filename);
+
+        operationService.addSuccessfulOperation(String.format("the %s file has been downloaded successfully", filename));
+
+        return result;
+
+    }
+
+
 }
